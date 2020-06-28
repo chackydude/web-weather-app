3b283f6cb5eaa3cd2ea778757bf94f17dcc445b8
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-06-28 12:45:34 +0300

    working prototype

--- web-weather-app/src/App.vue
+++ web-weather-app/src/App.vue
@@ -71,7 +71,8 @@ export default {
           },
           dateBuilder () {
               let d = new Date();
-              let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+              let months = ["January", "February", "March", "April", "May", "June", "July", "August",
+                            "September", "October", "November", "December"];
               let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
               let day = days[d.getDay()];
               let date = d.getDate();
---
+++ web-weather-app/src/components/WeatherItem.vue
@@ -0,0 +1,43 @@
+<template>
+        <div class="info-wrapper" v-if="typeof weather.main != 'undefined'">
+            <img v-bind:src="imgUrl" alt="">
+            <div class="temperature"> {{Math.round(weather.main.temp)}}°C</div>
+            <div class="location-info">
+                <div class="date"> {{ dateBuilder() }} </div>
+                <div class="locaton">{{ weather.weather[0].main }} in {{ weather.name }} </div>
+                <div class="description"> ({{ weather.weather[0].description }}) </div>
+            </div>
+            <div class="weather">
+
+                <div class="wind"> {{ weather.wind.speed }}mph</div>
+                <div class="humidity">{{ weather.main.humidity }}%</div>
+                <div class="pressure">{{ weather.main.pressure }}mb</div>
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "WeatherItem"
+    }
+</script>
+
+<style scoped>
+    .info-wrapper {
+        text-align: center;
+        font-size: 30px;
+        color: #fff;
+        text-shadow: 1px 1px black;
+        background-color:rgba(255, 255, 255, 0.4);
+        width: 300px;
+        padding: 20px;
+        border-radius: 20px;
+        margin: auto;
+    }
+
+    .temperature {
+        font-size: 50px;
+        margin-bottom: 10px;
+    }
+
+</style>
